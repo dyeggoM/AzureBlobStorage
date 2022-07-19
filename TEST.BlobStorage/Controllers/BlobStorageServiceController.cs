149917c74372eaf1b3c1c14bb98b4e72d8e2edc8
Juan Diego Morales del Corral <dmdelcor@gmail.com>
2022-07-18 21:57:19 -0500

    Added functionality for blob storage data management

---
+++ TEST.BlobStorage/Controllers/BlobStorageServiceController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TEST.BlobStorage.Services;
+
+namespace TEST.BlobStorage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlobStorageServiceController : ControllerBase
+    {
+        private IBlobStorageStorage _storage;
+        public BlobStorageServiceController(IBlobStorageStorage storage)
+        {
+            _storage = storage;
+        }
+
+        /// <summary>
+        /// Gets all blob names from blob storage.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetNames()
+        {
+            try
+            {
+                var names = await _storage.GetNames();
+                return Ok(names);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Gets all blob names containing spacified name.
+        /// </summary>
+        /// <param name="name">Name to find in blob names.</param>
+        /// <returns></returns>
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            try
+            {
+                var names = await _storage.GetNames();
+                var response = names.Where(x => x.Contains(System.Web.HttpUtility.UrlDecode(name)));
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Downloads a file from blob storage.
+        /// </summary>
+        /// <param name="blobName">Blob name to download.</param>
+        /// <returns></returns>
+        [HttpGet("download/{name}")]
+        public async Task<IActionResult> DownloadFile(string blobName)
+        {
+            try
+            {
+                var response = await _storage.Download(System.Web.HttpUtility.UrlDecode(blobName));
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Uploads a list of files to the blob storage.
+        /// </summary>
+        /// <param name="files">List of files to upload to blob storage.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Save(List<IFormFile> files)
+        {
+            try
+            {
+                if (files.Count < 1)
+                    return BadRequest();
+                var uris = new List<string>();
+                var guid = Guid.NewGuid().ToString("N").ToLower();
+                foreach (var file in files)
+                {
+                    var image = file.OpenReadStream();
+                    var blobName = $"{guid}/{file.FileName}";
+                    var uri = await _storage.Save(image, blobName);
+                    uris.Add(uri);
+                }
+                return Ok(uris);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500,e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes a file from blob storage.
+        /// </summary>
+        /// <param name="blobName">Blob name to delete (file path in blob storage).</param>
+        /// <returns></returns>
+        [HttpDelete("{blobName}")]
+        public async Task<IActionResult> Delete(string blobName)
+        {
+            try
+            {
+                var result = await _storage.Delete(System.Web.HttpUtility.UrlDecode(blobName));
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ TEST.BlobStorage/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TEST.BlobStorage.CustomEntities;
+using TEST.BlobStorage.Services;
+
+namespace TEST.BlobStorage
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<AzureStorageConfig>(Configuration.GetSection("AzureStorageConfig"));
+            services.AddSingleton<IBlobStorageStorage, BlobStorageService>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TEST.BlobStorage", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TEST.BlobStorage v1"));
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
